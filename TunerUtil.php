591a8f599b95300886802d24c49fc337c2dcce3a
Ido Green <greenido@gmail.com>
2018-04-11 19:13:47 -0700

    Tuning the new legs for 2018 run

--- TunerUtil.php
+++ TunerUtil.php
@@ -7,9 +7,12 @@
  */
 class Ggr {
 
-  private $fileName = "/Applications/MAMP/htdocs/relaymaps/index.html";
-  private $outputFile = "/Applications/MAMP/htdocs/relaymaps/index-o.html";
-  
+  // TODO: make sure you are working on the right input file
+  private $fileName = "/Applications/MAMP/htdocs/relaymaps/index-2018.html";
+  private $outputFile = "/Applications/MAMP/htdocs/relaymaps/index.html";
+  private $headHTML = "/Applications/MAMP/htdocs/relaymaps/head.html";
+  private $footHTML = "/Applications/MAMP/htdocs/relaymaps/footer.html";
+
   /**
    * Ctor
    */
@@ -20,7 +23,7 @@ class Ggr {
     $legs = 36; // we have 36 legs in the race
     $fullHtml = $this->getAllHtml();
     
-    for ($i = 2; $i < $legs+1; $i++) {
+    for ($i = 1; $i < $legs+1; $i++) {
       $inx1 = strpos($fullHtml, "<h1>Leg") - 7;
       if ($i > 2) {
         $fullHtml = substr_replace($fullHtml, '</div></div> <button class="btn btn-primary btn-space" type="button" data-toggle="collapse" 
@@ -35,7 +38,19 @@ class Ggr {
           Leg '.$i.'</button> <div class="collapse" id="leg-' . $i .'-details">
             <div class="card card-block">', $inx1, 15); 
       } 
+      echo "Replacing leg $i \n";
     }
+    // $headHTML = file_get_contents($this->headHTML);
+    // $inx4 = strpos($fullHtml, "<head>");
+    // $inx5 = strpos($fullHtml, "</head>", $inx4);
+    // $fullHtml = substr_replace($fullHtml, $headHTML, $inx4, ($inx5-$inx4) );
+
+    // $footerHtml = file_get_contents($this->footHTML);
+    // $inx6 = strpos($fullHtml, "</body>");
+    // $inx7 = strpos($fullHtml, "</html>", $inx6);
+    // $fullHtml = substr_replace($fullHtml, $footHTML, $inx6 , $inx7 - $inx6);
+
+    // let's output everyting to a new file
     file_put_contents($this->outputFile, $fullHtml);
   }
   
